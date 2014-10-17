f872e30b61041414bdd96d693cd62ccedc582434
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-10-17 14:15:27 -0500

    Moved cookbook nativex-python to python-nativex

---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: nativex-python
+# Recipe:: default
+#
+# Copyright 2014, NativeX
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe	"nativex-python::pippackages"
